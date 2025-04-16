b8000bfe1e152d0052fe8da8fccc26a1ae2146da
Mahir3008 <lodhiamahir@gmail.com>
2025-04-16 21:04:35 +0530

    Add project files.

---
+++ project/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtadd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.gdview = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gdview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(172, 299);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter Employee Id: ";
+            // 
+            // txtid
+            // 
+            this.txtid.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtid.Location = new System.Drawing.Point(242, 48);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(210, 30);
+            this.txtid.TabIndex = 2;
+            // 
+            // txtadd
+            // 
+            this.txtadd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtadd.Location = new System.Drawing.Point(242, 122);
+            this.txtadd.Multiline = true;
+            this.txtadd.Name = "txtadd";
+            this.txtadd.Size = new System.Drawing.Size(210, 66);
+            this.txtadd.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter Address :";
+            // 
+            // txtemail
+            // 
+            this.txtemail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtemail.Location = new System.Drawing.Point(702, 125);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(180, 30);
+            this.txtemail.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(496, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Enter Email Id :";
+            // 
+            // txtname
+            // 
+            this.txtname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtname.Location = new System.Drawing.Point(702, 51);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(180, 30);
+            this.txtname.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(496, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Enter Name :";
+            // 
+            // gdview
+            // 
+            this.gdview.AllowUserToAddRows = false;
+            this.gdview.AllowUserToDeleteRows = false;
+            this.gdview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gdview.Location = new System.Drawing.Point(172, 348);
+            this.gdview.Name = "gdview";
+            this.gdview.ReadOnly = true;
+            this.gdview.RowHeadersWidth = 51;
+            this.gdview.RowTemplate.Height = 24;
+            this.gdview.Size = new System.Drawing.Size(710, 218);
+            this.gdview.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(286, 299);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 43);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(410, 299);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 43);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+//            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1116, 578);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.gdview);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtadd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gdview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txtadd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView gdview;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ project/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+using System.Net;
+using System.Security.Cryptography;
+using System.Xml.Linq;
+namespace project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\lodhi\source\repos\project\project\Database1.mdf;Integrated Security=True");
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtid.Text != string.Empty && txtname.Text != string.Empty && txtemail.Text != string.Empty)
+                {
+                    conn.Open();
+                    string str = "insert into employeemst values(@eid, @name, @address, @email)";
+                    SqlCommand cmd = new SqlCommand(str, conn);
+                    cmd.Parameters.AddWithValue("@eid", txtid.Text);
+                    cmd.Parameters.AddWithValue("@name", txtname.Text);
+                    cmd.Parameters.AddWithValue("@address", txtadd.Text);
+                    cmd.Parameters.AddWithValue("@email", txtemail.Text);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Record inserted successfully", "Record Inserted",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    conn.Close();
+                    //bindgrid();
+
+                }
+                else
+                {
+                    MessageBox.Show("Please enter value in all fields", "Invalid data", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                // clearall();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            }
+ private void clearall()
+        {
+            txtid.Clear();
+            txtname.Clear();
+            txtadd.Clear();
+            txtemail.Clear();
+            txtid.ReadOnly = false;
+            txtid.Focus();
+        }
+        private void bindgrid()
+        {
+            try
+            {
+                string str = "select *from employeemst";
+                SqlDataAdapter adpt = new SqlDataAdapter(str, conn);
+                DataTable dt = new DataTable();
+                adpt.Fill(dt);
+                gdview.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bindgrid();
+        }
+        private void Gdview_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if (e.RowIndex >= 0)
+                {
+                    DataGridViewRow row = gdview.Rows[e.RowIndex];
+                    txtid.Text = row.Cells[0].Value.ToString();
+                    txtname.Text = row.Cells[1].Value.ToString();
+                    txtadd.Text = row.Cells[2].Value.ToString();
+                    txtemail.Text = row.Cells[3].Value.ToString();
+                    txtid.ReadOnly = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void Btnupdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtid.Text != string.Empty && txtname.Text != string.Empty &&
+               txtemail.Text != string.Empty)
+                {
+                    conn.Open();
+                    string str = "update employeemst set name = @name,address = @address,email = @email where eid = @eid";
+                    SqlCommand cmd = new SqlCommand(str, conn);
+                    cmd.Parameters.AddWithValue("@eid", txtid.Text);
+                    cmd.Parameters.AddWithValue("@name", txtname.Text);
+                    cmd.Parameters.AddWithValue("@address", txtadd.Text);
+                    cmd.Parameters.AddWithValue("@email", txtemail.Text);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Record updated successfully", "Record Updated",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    conn.Close();
+                    bindgrid();
+                }
+                else
+                {
+                    MessageBox.Show("Please enter value in all fields", "Invalid    data",
+
+
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            clearall();
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtid.Text != string.Empty)
+                {
+                    conn.Open();
+                    string str = "delete from employeemst where eid=@eid";
+                    SqlCommand cmd = new SqlCommand(str, conn);
+                    cmd.Parameters.AddWithValue("@eid", txtid.Text);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Record deleted successfully", "Record Deleted",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    conn.Close();
+                    bindgrid();
+                }
+                else
+                {
+                    MessageBox.Show("Please enter value in all fields", "Invalid data",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            clearall();
+
+        }
+
+    }
+}
